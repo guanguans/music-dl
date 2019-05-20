e6b6fc5d1483cd7278f779cbe6efca117a21d93e
琯琯 <yzmguanguan@gmail.com>
2019-05-20 18:09:01 +0800

    Add Exception

---
+++ src/Exception/Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the guanguans/music-php.
+ *
+ * (c) 琯琯 <yzmguanguan@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\MusicPhp\Exception;
+
+class Exception extends \Exception
+{
+}
