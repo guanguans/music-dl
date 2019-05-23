dadedcb6636ee67f7770c4da2a59d17264f9f65a
琯琯 <yzmguanguan@gmail.com>
2019-05-23 13:45:24 +0800

    Refactor config

---
+++ src/Helpers.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the guanguans/music-php.
+ *
+ * (c) 琯琯 <yzmguanguan@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+/**
+ * @param null $key
+ *
+ * @return mixed|null
+ */
+function config($key = '')
+{
+    foreach ([MUSIC_PHP_PATH.'/../../../guanguans/music-php/src/Config.php', MUSIC_PHP_PATH.'/../src/Config.php'] as $file) {
+        if (file_exists($file)) {
+            $config = require $file;
+            break;
+        }
+    }
+
+    if (!is_string($key)) {
+        return null;
+    }
+
+    if (empty($key)) {
+        return $config;
+    }
+
+    if (!strpos($key, '.')) {
+        return isset($config[$key]) ? $config[$key] : null;
+    }
+
+    $key = explode('.', $key);
+
+    return isset($config[$key[0]][$key[1]]) ? $config[$key[0]][$key[1]] : null;
+}
