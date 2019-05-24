a0385e08f24d22db09820d478809debc4d71cea3
琯琯 <yzmguanguan@gmail.com>
2019-05-24 17:24:44 +0800

    Add TestCase.php

---
+++ tests/TestCase.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of the guanguans/music-php.
+ *
+ * (c) 琯琯 <yzmguanguan@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    /**
+     * Tear down the test case.
+     */
+    public function tearDown()
+    {
+        $this->finish();
+        parent::tearDown();
+        if ($container = \Mockery::getContainer()) {
+            $this->addToAssertionCount($container->Mockery_getExpectationCount());
+        }
+        \Mockery::close();
+    }
+
+    /**
+     * Run extra tear down code.
+     */
+    protected function finish()
+    {
+        // call more tear down methods
+    }
+}
