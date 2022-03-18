ad11641f4b32d689e70954effff4640eed7ec294
guanguans <guanguans@users.noreply.github.com>
2022-03-18 05:55:47 +0000

    Fix styling

--- app/HttpClientFactoryInterface.php
+++ app/HttpClientFactoryInterface.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the guanguans/music-php.
+/**
+ * This file is part of the guanguans/music-dl.
  *
- * (c) 琯琯 <yzmguanguan@gmail.com>
+ * (c) guanguans <ityaozm@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled.
  */
