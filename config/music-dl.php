d51951de7583b420bbcd03994d46be105c8c59de
yaozm <ityaozm@gmail.com>
2022-03-18 20:00:04 +0800

    Rename resources/icon-success.png -> resources/success-icon.png

--- config/music-dl.php
+++ config/music-dl.php
@@ -49,5 +49,5 @@ logo,
     'download_failed_tips' => PHP_EOL.'下载失败：<fg=red>%s</>',
     'input_error' => '<fg=red>o(╥﹏╥)o 输入错误</>',
     'save_path_tips' => PHP_EOL.'已保存到：<fg=yellow>%s</>，请前往查看',
-    'success_icon' => resource_path('icon-success.png'),
+    'success_icon' => resource_path('success-icon.png'),
 ];
