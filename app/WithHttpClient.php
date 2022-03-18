4d0380f89181879c4d2b5804293dcea442498b47
guanguans <guanguans@users.noreply.github.com>
2022-03-18 07:38:15 +0000

    Fix styling

--- app/WithHttpClient.php
+++ app/WithHttpClient.php
@@ -1,9 +1,9 @@
 <?php
 
-/*
- * This file is part of the guanguans/music-php.
+/**
+ * This file is part of the guanguans/music-dl.
  *
- * (c) 琯琯 <yzmguanguan@gmail.com>
+ * (c) guanguans <ityaozm@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled.
  */
@@ -19,7 +19,7 @@ trait WithHttpClient
 
     public static function createHttpClient(array $config = []): ClientInterface
     {
-        if (!self::$httpClient instanceof ClientInterface || $config) {
+        if (! self::$httpClient instanceof ClientInterface || $config) {
             return self::$httpClient = new Client($config);
         }
 
