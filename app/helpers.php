0289c5978c8c4055894e5a36d8092d716c7625d4
guanguans <guanguans@users.noreply.github.com>
2022-03-18 07:39:52 +0000

    Fix styling

--- app/helpers.php
+++ app/helpers.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the guanguans/music-php.
+/**
+ * This file is part of the guanguans/music-dl.
  *
- * (c) 琯琯 <yzmguanguan@gmail.com>
+ * (c) guanguans <ityaozm@gmail.com>
  *
  * This source file is subject to the MIT license that is bundled.
  */
@@ -13,7 +13,7 @@ declare(strict_types=1);
 use App\Exceptions\RuntimeException;
 use Joli\JoliNotif\Util\OsHelper;
 
-if (!function_exists('get_song_download_dir')) {
+if (! function_exists('get_song_download_dir')) {
     /**
      * @throws \App\Exceptions\RuntimeException
      */
@@ -22,7 +22,7 @@ if (!function_exists('get_song_download_dir')) {
         $downloadDir = OsHelper::isWindows()
             ? sprintf('C:\\Users\\%s\\Downloads\\%s\\', get_current_user(), $dir)
             : sprintf('%s/Downloads/%s/', exec('cd ~; pwd'), $dir);
-        if (!is_dir($downloadDir) && !mkdir($downloadDir, 0777, true) && !is_dir($downloadDir)) {
+        if (! is_dir($downloadDir) && ! mkdir($downloadDir, 0777, true) && ! is_dir($downloadDir)) {
             throw new RuntimeException(sprintf('The directory "%s" was not created', $downloadDir));
         }
 
@@ -30,7 +30,7 @@ if (!function_exists('get_song_download_dir')) {
     }
 }
 
-if (!function_exists('get_song_save_path')) {
+if (! function_exists('get_song_save_path')) {
     /**
      * @throws \App\Exceptions\RuntimeException
      */
