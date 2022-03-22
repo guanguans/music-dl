09185ee34dcad1b80195aa79577d539b46605bc6
yaozm <ityaozm@gmail.com>
2022-03-22 12:49:25 +0800

    Update music-dl.bat

--- builds/music-dl.bat
+++ builds/music-dl.bat
@@ -1,2 +1,15 @@
-@ECHO off
-php %~dp0/music-dl %*
+@echo off
+
+rem --------------------------------------------
+rem  music-dl command for Windows.
+rem --------------------------------------------
+
+@setlocal
+
+set MUSIC_DL_PATH=%~dp0
+
+if "%PHP_COMMAND%" == "" set PHP_COMMAND=php.exe
+
+"%PHP_COMMAND%" "%MUSIC_DL_PATH%music-dl" %*
+
+@endlocal
