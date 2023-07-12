d7e40a6b4d577d5ea07726caf47e2ce31633f9aa
yaozm <ityaozm@gmail.com>
2023-07-12 16:24:26 +0800

    feat(tests): Add test to verify downloaded music exists
    - Use `File::files()` to check if files exist in the download directory
    - Skip test on non-Darwin operating systems
    - Depends on 'chore(tests): Clean download directory before searching and downloading music'

--- tests/Feature/MusicCommandTest.php
+++ tests/Feature/MusicCommandTest.php
@@ -11,16 +11,37 @@ declare(strict_types=1);
  */
 
 use App\Commands\MusicCommand;
+use Illuminate\Support\Facades\File;
 use Mockery\Exception\InvalidOrderException;
 
+beforeEach(function (): void {
+    $this->downloadDir = base_path('tests/Download');
+});
+
 it('can search and download music', function (): void {
+    File::cleanDirectory($this->downloadDir);
+
     $this
         ->artisan(MusicCommand::class, [
             'keyword' => '不只是南方',
             '--driver' => 'fork',
+            '--dir' => base_path('tests/Download'),
             '--sources' => 'netease',
         ])
         ->expectsConfirmation(config('music-dl.confirm_download'), 'yes')
         ->expectsQuestion(config('music-dl.download_choice_tip'), '下载所有歌曲')
         ->assertSuccessful();
-})->group(__DIR__, __FILE__)->throws(InvalidOrderException::class);
+})
+    ->group(__DIR__, __FILE__)
+    ->throws(InvalidOrderException::class)
+    ->skip(PHP_OS_FAMILY !== 'Darwin');
+
+it('can find downloaded music', function (): void {
+    expect(File::files($this->downloadDir))
+        ->toBeArray()
+        ->toBeTruthy()
+        ->not->toBeEmpty();
+})
+    ->group(__DIR__, __FILE__)
+    ->depends('it can search and download music')
+    ->skip(PHP_OS_FAMILY !== 'Darwin');
