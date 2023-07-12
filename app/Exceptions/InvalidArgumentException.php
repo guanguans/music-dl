fa153d960bb19e3bf292d2a68cd0edc496a1752e
yaozm <ityaozm@gmail.com>
2023-07-12 12:05:00 +0800

    feat(Exceptions): Add InvalidArgumentException class
    - Add new file InvalidArgumentException.php
    - Implement InvalidArgumentException class
    - Make it implement Throwable interface

---
+++ app/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/music-dl.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace App\Exceptions;
+
+use App\Contracts\Throwable;
+
+final class InvalidArgumentException extends \InvalidArgumentException implements Throwable
+{
+}
