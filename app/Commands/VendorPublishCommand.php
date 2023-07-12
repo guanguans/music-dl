0ff776025e114d25b72f7785ecf10aecba8d8503
yaozm <ityaozm@gmail.com>
2023-07-12 13:21:46 +0800

    refactor(VendorPublishCommand): make VendorPublishCommand class final
    - Change the VendorPublishCommand class to be final
    - Improve code maintainability and prevent subclassing

--- app/Commands/VendorPublishCommand.php
+++ app/Commands/VendorPublishCommand.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
 
 namespace App\Commands;
 
-class VendorPublishCommand extends \Illuminate\Foundation\Console\VendorPublishCommand
+final class VendorPublishCommand extends \Illuminate\Foundation\Console\VendorPublishCommand
 {
     /**
      * @psalm-suppress UndefinedInterfaceMethod
