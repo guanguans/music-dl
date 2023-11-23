937393b30b678970861928b07acb99cd73ae8233
yaozm <ityaozm@gmail.com>
2023-11-23 16:13:15 +0800

    feat(composer): Add laravel/facade-documenter
    - Add laravel/facade-documenter to composer.json
    - Add repository for facade-documenter
    - Update composer-normalize command
    - Update facade-update command

---
+++ facade.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the guanguans/music-dl.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+require __DIR__.'/vendor/autoload.php';
+
+require __DIR__.'/vendor/bin/facade.php';
