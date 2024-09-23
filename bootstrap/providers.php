5e9463d8dcdeb50e835441de6711e3a9303e9554
guanguans <ityaozm@gmail.com>
2024-09-23 16:19:35 +0800

    feat(concurrency): add concurrency service provider and config
    - Add Illuminate\Concurrency\ConcurrencyServiceProvider to providers.
    - Create a new configuration file for concurrency with default settings.
    - Define the default concurrency driver in the new config file.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -20,6 +20,7 @@ return [
     Illuminate\Translation\TranslationServiceProvider::class,
     // Illuminate\Validation\ValidationServiceProvider::class,
     // Illuminate\View\ViewServiceProvider::class,
+    Illuminate\Concurrency\ConcurrencyServiceProvider::class,
 
     // Application Service Providers...
     App\Providers\AppServiceProvider::class,
---
+++ config/concurrency.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2019-2024 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/music-dl
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Default Concurrency Driver
+    |--------------------------------------------------------------------------
+    |
+    | This option determines the default concurrency driver that will be used
+    | by Laravel's concurrency functions. By default, concurrent work will
+    | be sent to isolated PHP processes which will return their results.
+    |
+    | Supported: "process", "fork", "sync"
+    |
+    */
+
+    'driver' => env('CONCURRENCY_DRIVER', 'process'),
+];
