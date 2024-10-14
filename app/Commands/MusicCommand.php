b6933c21186053546af7f52ba619e8e62153e78f
guanguans <ityaozm@gmail.com>
2024-10-14 14:47:07 +0800

    fix(MusicCommand): trim stdin input to remove trailing spaces
    - Change `rtrim` to `trim` for stdin input to remove both leading and trailing spaces.
    - Close the stdin stream after reading to prevent resource leaks.
    - Ensure consistent handling of input regardless of whitespace.

--- app/Commands/MusicCommand.php
+++ app/Commands/MusicCommand.php
@@ -68,10 +68,9 @@ final class MusicCommand extends Command implements Isolatable
             ->when(windows_os(), static fn () => warning(__('windows_hint')))
             ->tap(static function () use (&$stdinKeyword): void {
                 if (($fstat = fstat(\STDIN)) && 0 < $fstat['size']) {
-                    $stdinKeyword = rtrim(stream_get_contents(\STDIN));
+                    $stdinKeyword = trim(stream_get_contents(\STDIN));
+                    fclose(\STDIN);
                 }
-
-                // fclose(\STDIN);
             })
             ->tap(function () use (&$keyword, $stdinKeyword): void {
                 $keyword = str($stdinKeyword ?: $this->argument('keyword') ?? text(
