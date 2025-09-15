88df2c26dec26347e6b167227a3d08a84d767ea6
guanguans <ityaozm@gmail.com>
2025-09-15 18:06:31 +0800

    refactor(Music.php): Update method annotations and linting rules
    - Refactored method annotations in Music.php for clarity and accuracy.
    - Adjusted linting configuration to enforce a shorter line length of 200 characters.
    - Added a new linting command for facade checks in composer.json.
    
    Signed-off-by: guanguans <ityaozm@gmail.com>

--- app/Facades/Music.php
+++ app/Facades/Music.php
@@ -1,5 +1,7 @@
 <?php
 
+/** @noinspection MissingParameterTypeDeclarationInspection */
+
 declare(strict_types=1);
 
 /**
@@ -16,22 +18,23 @@ namespace App\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \GuzzleHttp\Client createHttpClient(array $config = [])
- * @method static never dd(mixed ...$args)
+ * @method static \Illuminate\Support\Collection search(string $keyword, array $sources = [])
  * @method static void download(string $url, string $savedPath)
+ * @method static \App\Music setMeting(\App\Support\Meting $meting)
+ * @method static \App\Music setDriver(\Illuminate\Contracts\Concurrency\Driver $driver)
+ * @method static \App\Music setMinCallMicroseconds(int $minCallMicroseconds)
+ * @method static \App\Music|mixed when(\Closure|mixed|null $value = null, callable|null $callback = null, callable|null $default = null)
+ * @method static \App\Music|mixed unless(\Closure|mixed|null $value = null, callable|null $callback = null, callable|null $default = null)
+ * @method static never dd(mixed ...$args)
  * @method static \App\Music dump(mixed ...$args)
- * @method static void flushMacros()
- * @method static bool hasMacro(string $name)
+ * @method static \App\Music setHttpClient(\GuzzleHttp\Client|null $httpClient)
+ * @method static \GuzzleHttp\Client createHttpClient(array $config = [])
+ * @method static mixed withLocale(string $locale, \Closure $callback)
  * @method static void macro(string $name, object|callable $macro)
  * @method static void mixin(object $mixin, bool $replace = true)
- * @method static \Illuminate\Support\Collection search(string $keyword, array $sources = [])
- * @method static \App\Music setDriver(\Illuminate\Contracts\Concurrency\Driver $driver)
- * @method static \App\Music setMeting(\App\Support\Meting $meting)
- * @method static \App\Music setMinCallMicroseconds(int $minCallMicroseconds)
+ * @method static bool hasMacro(string $name)
+ * @method static void flushMacros()
  * @method static \Illuminate\Support\HigherOrderTapProxy|\App\Music tap(callable|null $callback = null)
- * @method static \App\Music|mixed unless(\Closure|mixed|null $value = null, callable|null $callback = null, callable|null $default = null)
- * @method static \App\Music|mixed when(\Closure|mixed|null $value = null, callable|null $callback = null, callable|null $default = null)
- * @method static mixed withLocale(string $locale, \Closure $callback)
  *
  * @see \App\Music
  */
