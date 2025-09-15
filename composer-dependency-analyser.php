4382d8e6afbe69e316525004af8791b765ab2370
guanguans <ityaozm@gmail.com>
2025-09-15 12:06:41 +0800

    chore(editorconfig): enhance coding style configuration
    - Add additional rules for specific file types including .bat and .go
    - Set max line length for commit messages and markdown files
    - Introduce a new .git-blame-ignore-revs file for better blame management
    - Create _ide_helper.php and composer-dependency-analyser.php for improved IDE support and dependency analysis
    - Add readme-lint script for validating README files
    
    Signed-off-by: guanguans <ityaozm@gmail.com>

---
+++ composer-dependency-analyser.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2019-2025 guanguans<ityaozm@gmail.com>
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ *
+ * @see https://github.com/guanguans/music-dl
+ */
+
+use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
+
+return (new Configuration)
+    ->addPathsToScan(
+        [
+            __DIR__.'/app/',
+            __DIR__.'/bootstrap/',
+            __DIR__.'/config/',
+            __DIR__.'/resources/',
+            __DIR__.'/tests/',
+        ],
+        false
+    )
+    ->addPathsToExclude([
+        __DIR__.'/tests',
+        // __DIR__.'/src/Support/Rectors',
+    ])
+    ->ignoreUnknownClasses([
+    ])
+    /** @see \ShipMonk\ComposerDependencyAnalyser\Analyser::CORE_EXTENSIONS */
+    ->ignoreErrorsOnExtensions(
+        [
+            // 'ext-pdo',
+            // 'ext-pcntl',
+        ],
+        [ErrorType::SHADOW_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackages(
+        [
+            'laminas/laminas-text',
+            'laravel-lang/common',
+            'laravel-zero/phar-updater',
+        ],
+        [ErrorType::UNUSED_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackages(
+        [
+            'composer/xdebug-handler',
+            'guzzlehttp/psr7',
+            'laravel-lang/config',
+            'laravel-lang/locale-list',
+            'laravel-lang/routes',
+            'laravel-zero/foundation',
+            'nunomaduro/laravel-console-summary',
+            'psr/http-message',
+            'psr/log',
+            'symfony/console',
+            'symfony/process',
+        ],
+        [ErrorType::SHADOW_DEPENDENCY]
+    )
+    ->ignoreErrorsOnPackages(
+        [
+            'intonate/tinker-zero',
+        ],
+        [ErrorType::DEV_DEPENDENCY_IN_PROD]
+    );
--- monorepo-builder.php
+++ monorepo-builder.php
@@ -1,5 +1,7 @@
 <?php
 
+/** @noinspection PhpUnusedAliasInspection */
+
 declare(strict_types=1);
 
 /**
@@ -29,6 +31,7 @@ use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
 return static function (MBConfig $mbConfig): void {
     require __DIR__.'/vendor/autoload.php';
     $mbConfig->defaultBranch('master');
+    MBConfig::disableDefaultWorkers();
 
     /**
      * release workers - in order to execute.
