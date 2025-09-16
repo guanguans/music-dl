bf471d5326b5398188e6a9e30a917e45fffe5749
guanguans <ityaozm@gmail.com>
2025-09-16 15:46:11 +0800

    refactor(application): Enhance OutputStyle and ConsoleLogger initialization
    - Improve the initialization of OutputStyle to handle missing argv indexes.
    - Configure verbosity for console output based on debug options.
    - Add ConsoleLogger as a singleton using the newly created OutputStyle.
    
    Signed-off-by: guanguans <ityaozm@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,10 @@ use Illuminate\Log\LogManager;
 use Intonate\TinkerZero\TinkerZeroServiceProvider;
 use LaravelZero\Framework\Application;
 use Psr\Log\LoggerInterface;
+use RectorPrefix202509\Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Application as ConsoleApplication;
 use Symfony\Component\Console\Input\ArgvInput;
+use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 return Application::configure(basePath: \dirname(__DIR__))
@@ -31,10 +34,39 @@ return Application::configure(basePath: \dirname(__DIR__))
     ->booting(static function (Application $app): void {
         $app->singleton(Music::class);
         $app->bind(MusicContract::class, Music::class);
-
+    })
+    ->booting(static function (Application $app): void {
+        /**
+         * @see \Rector\Console\Style\SymfonyStyleFactory
+         */
         $app->singletonIf(
             OutputStyle::class,
-            static fn (): OutputStyle => new OutputStyle(new ArgvInput, new ConsoleOutput)
+            static function (): OutputStyle {
+                // to prevent missing argv indexes
+                /** @noinspection GlobalVariableUsageInspection */
+                if (!isset($_SERVER['argv'])) {
+                    /** @noinspection GlobalVariableUsageInspection */
+                    $_SERVER['argv'] = [];
+                }
+
+                $argvInput = new ArgvInput;
+                $consoleOutput = new ConsoleOutput;
+
+                // to configure all -v, -vv, -vvv options without memory-lock to Application run() arguments
+                (fn () => $this->configureIO($argvInput, $consoleOutput))->call(new ConsoleApplication);
+
+                // --debug is called
+                if ($argvInput->hasParameterOption('--debug')) {
+                    $consoleOutput->setVerbosity(OutputInterface::VERBOSITY_DEBUG);
+                }
+
+                return new OutputStyle($argvInput, $consoleOutput);
+            }
+        );
+
+        $app->singleton(
+            ConsoleLogger::class,
+            static fn (Application $app): ConsoleLogger => new ConsoleLogger($app->make(OutputStyle::class))
         );
     })
     ->booting(static function (Application $app): void {
